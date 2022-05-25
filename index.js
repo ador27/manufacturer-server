1a1b64c6244c4c03cda8f20b27feb86f00d34a9c
ador <rashedulhaque111@gmail.com>
2022-05-25 23:40:44 +0600

    user add to database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const res = require('express/lib/response');
@@ -21,6 +22,7 @@ async function run() {
         await client.connect();
         const toolCollection = client.db('affinity_electronics').collection('tools');
         const reviewCollection = client.db('affinity_electronics').collection('reviews');
+        const userCollection = client.db('affinity_electronics').collection('users');
 
 
         app.get('/tools', async (req, res) => {
@@ -36,6 +38,18 @@ async function run() {
             const review = await cursor.toArray();
             res.send(review);
         });
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
 
 
         app.post('/review', async (req, res) => {
